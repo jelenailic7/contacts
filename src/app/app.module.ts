b9b0dce195c335433e087ab3ade7ee03b9417a30
jelenailic7 <ilic.jelena20@gmail>
2018-01-09 18:16:57 +0100

    7 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent
+    ContactListComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
