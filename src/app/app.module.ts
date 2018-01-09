d2db94e346c722bb2b299ef8ab0644491c0f07a0
jelenailic7 <ilic.jelena20@gmail>
2018-01-09 18:38:26 +0100

    8 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    LayoutComponent
+    LayoutComponent,
+    NavBarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
